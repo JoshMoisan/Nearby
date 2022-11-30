39a2553a04139198fa29bbd70fb0ce1cf29ed7af
joshmoisan <josh.moisan@hotmail.fr>
2022-11-30 12:09:00 +0100

    fixup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+require "open-uri"
+
+puts 'Destroying previous users and items 🌱'
+
+Booking.destroy_all
+Item.destroy_all
+User.destroy_all
+# Review.destroy_all
+
+puts 'Seeding db 🌱'
+
+5.times do
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    description: Faker::Lorem.sentence(word_count: 10),
+    password: "123456",
+    address: Faker::Address.city,
+    profile_picture: Faker::LoremFlickr.image,
+  )
+  3.times do
+    item = Item.new(
+      name: Faker::Appliance.equipment,
+      category: "Tools",
+      condition: ["damage", "used", "like new"].sample,
+      description: Faker::Lorem.sentence(word_count: 15),
+      item_picture: Faker::LoremFlickr.image,
+      price: [1, 2 ,5 ,10 ,25].sample,
+      user_id: user.id
+    )
+                    # PROBLEM WITH THE UNSPLASH API
+    # file = URI.open("https://source.unsplash.com/random/?tool")
+    # item.photo.attach(io: file, filename: "name.png", content_type: "image/png")
+    item.save
+  end
+  puts "User #{user.first_name} has been created with an item"
+end
+
+puts 'Finished ✅'
