5a2cc2f7a5500838507a16271eb9dc11a65d624b
julian gaspar <juliangaspar15@gmail.com>
2022-11-30 12:29:49 +0100

    Validations on item/booking/user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Pending validations on pictures (id, profile)
+  validates :email, :first_name, :last_name, :username, :address
+  validates :username, uniqueness: true
 end
