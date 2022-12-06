677b1ed41ded940a5a510800ec24f1f338591bb6
julian gaspar <juliangaspar15@gmail.com>
2022-12-06 12:57:44 +0100

    Booking plus details

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -8,7 +8,6 @@ export default class extends Controller {
   }
 
   connect() {
-    console.log("heheheheh")
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
---
+++ app/javascript/controllers/pricing_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="pricing"
+export default class extends Controller {
+  static targets = ["price", "days"]
+
+  connect() {
+    this.priceTarget.dataset.price
+  }
+
+  pricePreview() {
+    const price = this.priceTarget.dataset.price
+    const duration = this.daysTarget.value
+    const bookingPrice = price * duration
+  }
+
+}
